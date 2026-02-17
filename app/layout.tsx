a419b6e89eac2bb6ef0d14d190a06c8b41f978eb
VladimirVranesevic <vladimir.vranesevic4355@gmail.com>
2026-02-17 11:32:15 +1100

    removed button

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import BackButton from "./Components/UI/BackButton"
 
 /*Font variables*/
 //defines geistSansn function which returns a font object from the Geist font, but only the latin portion.
@@ -44,7 +43,6 @@ export default function RootLayout({
     <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
     >
-        <BackButton />
         {children}
     </body>
     </html>
